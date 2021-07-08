26748ba379c9df6a88fbefe02ecd483faab95c24
megha-prasad <76677241+megha-prasad@users.noreply.github.com>
2021-07-08 21:57:58 +0530

    Create Python_training.py

---
+++ Python_training.py
@@ -0,0 +1,40 @@
+# --> Arithmetic operators :
+
+#   + Addition
+#   - Subtraction 
+#   * Multiplication
+#   / Division
+#   % Mod (the remainder after dividing)
+#   ** Exponentiation (note that ^ does not do this operation, as you might have seen in other languages)
+#   // Divides and rounds down to the nearest integer
+
+# *Also there are bitwise operators.
+# --> Quiz: Average Electricity Bill
+
+# My electricity bills for the last three months have been $23, $32 and $64. What is the average monthly electricity bill over the three month period? Write an expression to calculate the mean, and use print() to view the result.
+
+print((32+23+64)/3)
+print(32+64+23)
+
+# -->Quiz: Calculate
+# Two parts of a floor need tiling. One part is 9 tiles wide by 7 tiles long, the other is 5 tiles wide by 7 tiles long. Tiles come in packages of 6.
+
+# How many tiles are needed?
+# You buy 17 packages of tiles containing 6 tiles each. How many tiles will be left over?
+
+required = ((9*7) + (5*7))      # Fill this in with an expression that calculates how many tiles are needed.
+print(required)
+
+leftov = (17*6)-required        # Fill this in with an expression that calculates how many tiles will be left over.
+print(leftov)
+
+# --> Variables : 
+mv_population = 74728
+
+# Here mv_population is a variable, which holds the value of 74728. 
+# This assigns the item on the right to the name on the left, which is actually a little different than mathematical equality, as 74728 does not hold the value of mv_population.
+# whatever term is on the left side, is now a name for whatever value is on the right side. 
+# Once a value has been assigned to a variable name, you can access the value from the variable name.
+
+
+
