873717354990f50cb5fadbcedcf1b61cfce662d9
megha-prasad <76677241+megha-prasad@users.noreply.github.com>
2021-07-13 23:46:44 +0530

    Update Python_training_day5.py

--- Python_training_day5.py
+++ Python_training_day5.py
@@ -70,3 +70,51 @@ print(product)
 # When you want to loop based on receiving specific user input.
 # while user_input == 'y':
 
+# -->Break, Continue
+# Sometimes we need more control over when a loop should end, or skip an iteration. 
+# In these cases, we use the break and continue keywords, which can be used in both for and while loops.
+
+# break terminates a loop
+# continue skips one iteration of a loop
+# the code breaks the loop when weight exceeds or reaches the limit
+
+# example
+manifest = [("bananas", 15), ("mattresses", 24), ("dog kennels", 42), ("machine", 120), ("cheeses", 5)]
+
+print("METHOD 1")
+weight = 0
+items = []
+for cargo_name, cargo_weight in manifest:
+    print("current weight: {}".format(weight))
+    if weight >= 100:
+        print("  breaking loop now!")
+        break
+    else:
+        print("  adding {} ({})".format(cargo_name, cargo_weight))
+        items.append(cargo_name)
+        weight += cargo_weight
+
+print("\nFinal Weight: {}".format(weight))
+print("Final Items: {}".format(items))
+
+# skips an iteration when adding an item would exceed the limit
+# breaks the loop if weight is exactly the value of the limit
+print("\nMETHOD 2")
+weight = 0
+items = []
+for cargo_name, cargo_weight in manifest:
+    print("current weight: {}".format(weight))
+    if weight >= 100:
+        print("  breaking from the loop now!")
+        break
+    elif weight + cargo_weight > 100:
+        print("  skipping {} ({})".format(cargo_name, cargo_weight))
+        continue
+    else:
+        print("  adding {} ({})".format(cargo_name, cargo_weight))
+        items.append(cargo_name)
+        weight += cargo_weight
+
+print("\nFinal Weight: {}".format(weight))
+print("Final Items: {}".format(items))
+
