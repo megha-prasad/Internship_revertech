f282035c0936727378881bf78ed467e0fc3d5ace
megha-prasad <76677241+megha-prasad@users.noreply.github.com>
2021-07-13 11:45:01 +0530

    Update Python_training_day4.py

--- Python_training_day4.py
+++ Python_training_day4.py
@@ -235,3 +235,43 @@ Actor: Jason Alexander    Role: George Costanza
 Actor: Michael Richards    Role: Cosmo Kramer
 # items is an awesome method that returns tuples of key, value pairs, which you can use to iterate over dictionaries in for loops.
 
+#--> Quiz
+# You would like to count the number of fruits in your basket. 
+# In order to do this, you have the following dictionary and list of
+# fruits.  Use the dictionary and list to count the total number
+# of fruits, but you do not want to count the other items in your basket.
+
+result = 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+for keys, values in basket_items.items():
+    if keys in fruits:
+        result+=values#Iterate through the dictionary
+    else:
+        result = result
+
+#if the key is in the list of fruits, add the value (number of fruits) to result
+
+
+print(result)
+
+#--> Quiz
+# You would like to count the number of fruits in your basket. 
+# In order to do this, you have the following dictionary and list of
+# fruits.  Use the dictionary and list to count the total number
+# of fruits and not_fruits.
+
+fruit_count, not_fruit_count = 0, 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+for keys, values in basket_items.items():
+    if keys in fruits:
+        fruit_count+=values
+    elif keys not in fruits:
+        not_fruit_count += values
+
+
+print(fruit_count, not_fruit_count)
+
