ff5f27a07fdc4dc367aa29e6242c0e54f6356561
megha-prasad <76677241+megha-prasad@users.noreply.github.com>
2021-08-27 19:53:53 +0530

    Create Flask_training.py

---
+++ Flask_training.py
@@ -0,0 +1,5 @@
+# Flask is a web framework that provides libraries to build lightweight web applications in python. It is developed by Armin Ronacher who leads an international group of python enthusiasts (POCCO).
+# Flask is a web framework that provides libraries to build lightweight web applications in python. It is developed by Armin Ronacher who leads an international group of python enthusiasts (POCCO). It is based on WSGI toolkit and jinja2 template engine. Flask is considered as a micro framework.
+# WSGI- It is an acronym for web server gateway interface which is a standard for python web application development. It is considered as the specification for the universal interface between the web server and web application.
+# Jinja2 is a web template engine which combines a template with a certain data source to render the dynamic web pages.
+
